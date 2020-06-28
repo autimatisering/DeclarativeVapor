7801c7c041344fee055dcbfa069e11bcc699fa96
Joannis Orlandos <joannis@orlandos.nl>
2020-06-28 13:03:23 +0200

    Removeold mongodb

--- Sources/Example/main.swift
+++ Sources/Example/main.swift
@@ -2,7 +2,6 @@ import DeclarativeAPI
 import FluentMongoDriver
 
 let app = try Application(.detect())
-try app.connectMongoDB(to: "mongodb://localhost/decl")
 try app.databases.use(.mongo(connectionString: "mongodb://localhost/decl"), as: .mongo)
 
 app.register(CreateUser())
