5eca146e6578306b2f86bc620e49518afce3cb50
Joannis Orlandos <joannis@orlandos.nl>
2020-06-28 22:02:10 +0200

    Custom JSON encoding

--- Sources/Example/JSON.swift
+++ Sources/Example/JSON.swift
@@ -1,10 +1,9 @@
 import DeclarativeAPI
-import IkigaJSON
 
 @_functionBuilder
 public struct JSONObjectBuilder {
-    public static func buildBlock() -> IkigaJSON.JSONObject {
-        return IkigaJSON.JSONObject()
+    public static func buildBlock() -> EventualJSONObject {
+        EventualJSONObject(values: [])
     }
     
     public static func buildBlock(_ content: JSONKey...) -> EventualJSONObject {
@@ -16,8 +15,42 @@ public func JSONObject(@JSONObjectBuilder _ build: () -> EventualJSONObject) -> 
     build()
 }
 
+public struct _FinalizedJSONObject: Encodable {
+    fileprivate let pairs: [String: _EncodeFunction]
+    
+    private struct CustomKey: CodingKey {
+        init?(intValue: Int) {
+            nil
+        }
+        
+        init(stringValue: String) {
+            self.stringValue = stringValue
+        }
+        
+        var stringValue: String
+        var intValue: Int? { nil }
+    }
+    
+    private struct _EncodeContainer: Encodable {
+        let _encode: _EncodeFunction
+        
+        func encode(to encoder: Encoder) throws {
+            try _encode(encoder)
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CustomKey.self)
+        
+        for (key, value) in pairs {
+            let key = CustomKey(stringValue: key)
+            try container.encode(_EncodeContainer(_encode: value), forKey: key)
+        }
+    }
+}
+
 public struct EventualJSONObject: RouteResponse {
-    public typealias E = JSONObject
+    public typealias E = _FinalizedJSONObject
     let values: [JSONKey]
     
     public func encode(for request: Request) -> EventLoopFuture<E> {
@@ -25,27 +58,27 @@ public struct EventualJSONObject: RouteResponse {
             value.jsonValue(for: request)
         }
         
-        return EventLoopFuture.whenAllSucceed(pairs, on: request.eventLoop).map { pairs -> JSONObject in
-            var object = JSONObject()
+        return EventLoopFuture.whenAllSucceed(pairs, on: request.eventLoop).map { pairs -> _FinalizedJSONObject in
+            var object = [String: _EncodeFunction]()
             
             for (key, value) in pairs {
                 assert(!object.keys.contains(key), "Duplicate key in JSONObject, will overwrite key \"\(key)\"")
                 object[key] = value
             }
             
-            return object
+            return _FinalizedJSONObject(pairs: object)
         }
     }
 }
 
-public protocol EventualJSONPair {
-    func jsonValue(for request: Request) -> EventLoopFuture<(String, JSONValue)>
+fileprivate protocol EventualJSONPair {
+    func jsonValue(for request: Request) -> EventLoopFuture<(String, _EncodeFunction)>
 }
 
 public struct JSONKey: EventualJSONPair {
-    private var makeValue: (Request) -> EventLoopFuture<(String, JSONValue)>
+    private var makeValue: (Request) -> EventLoopFuture<(String, _EncodeFunction)>
     
-    public func jsonValue(for request: Request) -> EventLoopFuture<(String, JSONValue)> {
+    public func jsonValue(for request: Request) -> EventLoopFuture<(String, _EncodeFunction)> {
         makeValue(request)
     }
     
@@ -53,22 +86,11 @@ public struct JSONKey: EventualJSONPair {
         let value = build()
         self.makeValue = { request in
             value.encode(for: request).flatMapThrowing { value in
-                let value = try IkigaJSONEncoder().encodeJSONValue(from: value)
-                return (key, value)
+                return (key, value.encode)
             }
         }
 //        build().
     }
 }
 
-extension IkigaJSONEncoder {
-    public func encodeJSONValue<E: Encodable>(from encodable: E) throws -> JSONValue {
-        fatalError()
-    }
-}
-
-extension JSONObject: Encodable {
-    public func encode(to encoder: Encoder) throws {
-        fatalError()
-    }
-}
+public typealias _EncodeFunction = (Encoder) throws -> ()
